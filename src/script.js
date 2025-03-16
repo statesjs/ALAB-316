1c27d9a63638664b4e4648b75335c935e22310cc
statesjs <statesjs@gmail.com>
2025-03-16 10:16:55 -0700

    finished the password part n refactored

--- src/script.js
+++ src/script.js
@@ -1,39 +1,71 @@
 let usernameEl = document.getElementById("username");
 let emailEl = document.getElementById("email");
+let pass = document.getElementById("password");
 let formEl = document.getElementById("registration");
 let errorBox = document.getElementById("errorDisplay");
-
+let passCheck = document.getElementById("passwordCheck");
 formEl.addEventListener("submit", (e) => {
   let messages = []; // to collect and push to errorbox
-  let user = usernameEl.value.trim(); //
-  let email = emailEl.value.trim(); // email to string
+  const USER = usernameEl.value.trim(); //
+  const EMAIL = emailEl.value.trim(); // email to string
+  const PASS = passwordEl.value.trim(); //password
+  const PASSCHECK = passCheck.value.trim(); //password check
 
   //user name section
-  if (user === "") {
+  if (USER === "") {
     messages.push("Username is required.");
   } else {
-    if (user.length < 4) {
+    if (USER.length < 4) {
       messages.push("Username must be at least 4 characters long.");
     }
     // Ensure at least two unique characters using a Set
-    if (new Set(user).size < 2) {
+    if (new Set(USER).size < 2) {
       messages.push("Username must have at least 2 unique characters.");
     }
-    if (!/^[a-zA-Z0-9]+$/.test(user)) {
+    if (!/^[a-zA-Z0-9]+$/.test(USER)) {
       messages.push("Username must not contain special characters or spaces.");
     }
   }
 
   //email section
-  if (email === "") {
-    messages.push("Email is empty");
+  if (EMAIL === "") {
+    messages.push("Email is empty.");
   } else {
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)) {
       messages.push("Email is not a valid address");
     }
     // Ensure email is NOT from "example.com"
-    else if (email.toLowerCase().endsWith("@example.com")) {
+    else if (EMAIL.toLowerCase().endsWith("@example.com")) {
       messages.push("Email contains example.com");
+    }
+  }
+
+  if (PASS === "") {
+    messages.push("Password is empty");
+  } else {
+    if (PASS.length <= 12) {
+      //12 characters at least
+      messages.push("Password must be at least 12 characters long.");
+    }
+    //one upper one lower
+    else if (/[A-Z]/.test(PASS) && /[a-z]/.test(PASS)) {
+      //lower and uppercase
+      messages.push(
+        "Password must contain at least one uppercase and one lowercase character."
+      );
+    } else if (/[0-9]/.test(PASS)) {
+      // one number needed
+      messages.push("Password requires at least one number.");
+    } else if (!/[^a-zA-Z0-9]/.test(PASS)) {
+      //special character test
+      messages.push("Password cannot contain special characters.");
+    } else if (PASS.toLowerCase().includes("password")) {
+      //password cannot contain password, wtf does "mixed" mean?
+      messages.push('Password cannot contain "password"');
+    } else if (PASS.toLowerCase().includes(USER.toLowerCase)) {
+      messages.push("Password cannot include the username.");
+    } else if (PASS !== PASSCHECK) {
+      messages.push("Passwords do not match.");
     }
   }
 
